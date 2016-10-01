05d203c6b1ee20e9387a27f73642412f0020a8b1
redangel1993 <Kamperis1993@hotmail.com>
2016-10-01 20:26:03 +0300

    Initial commit

---
+++ app/src/androidTest/java/suitapp/com/marioskamperis/suitapp/ApplicationTest.java
@@ -0,0 +1,13 @@
+package suitapp.com.marioskamperis.suitapp;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
